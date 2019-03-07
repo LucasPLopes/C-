2bac7a209a9933296b5ad94a462f6957117480bb
lucas lopes <l.lopes.fga@gmail.com>
2019-03-07 00:39:54 -0300

    Adicionado os métodos de construtor , de incremento pré-fixado e print

--- TreinamentoCPPvol2/Ponto.cpp
+++ TreinamentoCPPvol2/Ponto.cpp
@@ -3,14 +3,32 @@
 using namespace std;
 
 class Ponto{
-	
-	
-	
-};
+	private:
+		int x,y;
+	public:
 
+	Ponto( int xx = 0, int yy =0 ): x(xx), y(yy){}
+	void operator ++();// ++P
+	// void operator ++(int).//P++
+	void print() const;
+};
+void Ponto::operator++(){
+	++x;
+	++y;
+}
+void Ponto::print()const{
+	cout<<"X: "<<x<<" Y: "<<y<<endl;
+}
 
 int main(){
-	
+	Ponto A, B(2,3), *C;
+	cout<<"A ";
+	A.print();
+	++A;
+	cout<<"A ";
+	A.print();
+	cout<<"B ";
+	B.print();
 	
 	return 0;
 }
