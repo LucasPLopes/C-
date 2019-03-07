aa040c0a878355a45e2a1c70d09e3b24a13772df
lucas lopes <l.lopes.fga@gmail.com>
2019-03-07 00:51:45 -0300

    modificado o metodo de incremento para retornar uma cópia do objeto e mais exemplos com incremento alterado

--- TreinamentoCPPvol2/Ponto.cpp
+++ TreinamentoCPPvol2/Ponto.cpp
@@ -9,23 +9,24 @@ class Ponto{
 
 	Ponto( int xx = 0, int yy =0 ): x(xx), y(yy){}
 	~Ponto(){}
-	void operator ++();// ++P
+	// void operator ++();// ++P
+	Ponto operator ++();
 	// void operator ++(int).//P++
 	void print() const;
 };
-void Ponto::operator++(){
-	++x;
-	++y;
+Ponto Ponto::operator++(){
+	return Ponto(++x,++y);
+	
 }
 void Ponto::print()const{
 	cout<<"X: "<<x<<" Y: "<<y<<endl;
 }
 
 int main(){
-	Ponto A, B(2,3), *C;
+	Ponto A, B(2,3), *C,D;
 	cout<<"A ";
 	A.print();
-	++A;
+	++A;  // é equivalente a A.operator++();
 	cout<<"A ";
 	A.print();
 	cout<<"B ";
@@ -34,6 +35,10 @@ int main(){
 	++(*C);
 	cout<<"C ";
 	C->print();
+	D= ++B;
+	cout<<"D ";
+	D.print();
+	
 	return 0;
 }
 
