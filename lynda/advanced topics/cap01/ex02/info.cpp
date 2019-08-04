09fae2faf3437e19f98688890061198301e63035
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-04 13:49:40 -0300

    Criaçao de setters

--- lynda/advanced topics/cap01/ex02/info.cpp
+++ lynda/advanced topics/cap01/ex02/info.cpp
@@ -10,12 +10,19 @@ public:
     int getA(){return a;}
     const char * getB(){return b;}
     double getC(){return c;}
+    void setA(const int a){this->a = a;}
+    void setB(const char* b){this->b = b;}
+    void setC(const double c){this->c = c; }
 
 };
 
 
 int main(){
-    Info * info = new Info(23,"Lucas Lopes",1500);
+    Info * info = new Info(0,"L4cas Lopes",1500);
+    
+    info->setA(23);
+    info->setB("Lucas Lopes");
+
 
     cout<<"Nome: "<<info->getB()<<endl;
     cout<<"Idade: "<<info->getA()<<endl;
